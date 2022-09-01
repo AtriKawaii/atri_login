2843bb9ab91d7d3f16d2aec2d5d3b0821cf88cae
LaoLittle <93422095+LaoLittle@users.noreply.github.com>
2022-09-01 19:27:55 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-mod compat;
-
 extern crate core;
 
-use std::{env};
+use std::env;
 use std::error::Error;
 use std::fmt::Debug;
 use std::path::{Path, PathBuf};
@@ -11,22 +9,21 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use bytes::Bytes;
-use qrcode::QrCode;
 use qrcode::render::unicode;
-use ricq::{Client, LoginResponse, QRCodeConfirmed, QRCodeImageFetch, QRCodeState, version};
+use qrcode::QrCode;
 use ricq::client::Token;
 use ricq::device::Device;
 use ricq::ext::common::after_login;
 use ricq::handler::DefaultHandler;
-use tokio::{fs, io, runtime};
+use ricq::{version, Client, LoginResponse, QRCodeConfirmed, QRCodeImageFetch, QRCodeState};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpSocket;
 use tokio::task::yield_now;
-use tracing::{debug, error, Event, Id, info, Level, Metadata, Subscriber};
+use tokio::{fs, io, runtime};
 use tracing::span::{Attributes, Record};
+use tracing::{debug, error, info, Event, Id, Level, Metadata, Subscriber};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use crate::compat::mirai::MiraiDeviceInfo;
 
 static HELP_INFO: &str = "\
 RQ Login Helper
@@ -78,11 +75,9 @@ type MainResult = Result<(), Box<dyn Error>>;
 
 fn main() -> MainResult {
     tracing_subscriber::registry()
-        .with(tracing_subscriber::filter::filter_fn(|m| {
-            match m.level() {
-                &Level::TRACE => false,
-                _ => true
-            }
+        .with(tracing_subscriber::filter::filter_fn(|m| match m.level() {
+            &Level::TRACE => false,
+            _ => true,
         }))
         .with(tracing_subscriber::fmt::layer().with_target(true))
         .init();
@@ -102,8 +97,7 @@ async fn main0() -> MainResult {
     let stdin = tokio::io::stdin();
     let mut stdin = BufReader::new(stdin);
 
-    'main:
-    loop {
+    'main: loop {
         //print!(">>");
         stdout.write_all(b">>").await?;
         stdout.flush().await?;
@@ -124,7 +118,9 @@ async fn main0() -> MainResult {
             "login" => {
                 let account = unwrap_option_or_help!(spl.get(1));
                 p.push(account);
-                if !p.is_dir() { fs::create_dir(&p).await?; }
+                if !p.is_dir() {
+                    fs::create_dir(&p).await?;
+                }
 
                 let account = unwrap_result_or_err!(i64::from_str(account));
 
@@ -135,7 +131,6 @@ async fn main0() -> MainResult {
 
                 let resp = client.password_login(account, password).await?;
 
-
                 println!("{:?}", resp);
             }
             "qrlogin" => {
@@ -143,12 +138,12 @@ async fn main0() -> MainResult {
                     Some(account) => {
                         unwrap_result_or_err!(i64::from_str(account));
                         p.push(account);
-                        if !p.is_dir() { fs::create_dir(&p).await?; }
+                        if !p.is_dir() {
+                            fs::create_dir(&p).await?;
+                        }
                         device_or_default(&p).await
                     }
-                    None => {
-                        Device::random()
-                    }
+                    None => Device::random(),
                 };
 
                 let client = get_client(device).await?;
@@ -160,11 +155,10 @@ async fn main0() -> MainResult {
                 let mut signature = None::<Bytes>;
                 loop {
                     match state {
-                        QRCodeState::ImageFetch(
-                            QRCodeImageFetch {
-                                ref image_data,
-                                ref sig
-                            }) => {
+                        QRCodeState::ImageFetch(QRCodeImageFetch {
+                            ref image_data,
+                            ref sig,
+                        }) => {
                             img_file.write_all(image_data).await?;
                             signature = Some(sig.clone());
                             let mut qr_file = env::current_dir().unwrap_or_default();
@@ -182,28 +176,27 @@ async fn main0() -> MainResult {
                         QRCodeState::WaitingForConfirm => {
                             info!("已扫码，等待确认");
                         }
-                        QRCodeState::Confirmed(
-                            QRCodeConfirmed {
-                                ref tmp_pwd,
-                                ref tmp_no_pic_sig,
-                                ref tgt_qr,
-                                ..
-                            }) => {
-                            let mut login_resp = client.qrcode_login(
-                                tmp_pwd,
-                                tmp_no_pic_sig,
-                                tgt_qr,
-                            ).await?;
+                        QRCodeState::Confirmed(QRCodeConfirmed {
+                            ref tmp_pwd,
+                            ref tmp_no_pic_sig,
+                            ref tgt_qr,
+                            ..
+                        }) => {
+                            let mut login_resp =
+                                client.qrcode_login(tmp_pwd, tmp_no_pic_sig, tgt_qr).await?;
 
-                            if let LoginResponse::DeviceLockLogin(..) = login_resp {
-                                login_resp = client.device_lock_login().await?;
-                            } else {
-                                panic!("Not device lock login: {:?}", login_resp);
+                            match login_resp {
+                                LoginResponse::DeviceLockLogin(..) => {
+                                    login_resp = client.device_lock_login().await?;
+                                }
+                                LoginResponse::Success(..) => {}
+                                _ => {
+                                    panic!("Unknown status: {:?}", login_resp);
+                                }
                             }
 
                             info!("登陆成功");
                             debug!("{:?}", login_resp);
-
                             break;
                         }
                         QRCodeState::Canceled => {
@@ -236,7 +229,9 @@ async fn main0() -> MainResult {
 
                 let account = client.uin().await;
                 p.push(account.to_string());
-                if !p.is_dir() { fs::create_dir(&p).await?; }
+                if !p.is_dir() {
+                    fs::create_dir(&p).await?;
+                }
                 {
                     p.push("device.json");
                     let mut f = fs::File::create(&p).await?;
@@ -245,20 +240,15 @@ async fn main0() -> MainResult {
                     let s = serde_json::to_string_pretty(&device)?;
                     f.write_all(s.as_bytes()).await?;
                     p.pop();
-
-                    p.push("device.mirai.json");
-                    let mut f = fs::File::create(&p).await?;
-
-                    let mirai: MiraiDeviceInfo = device.into();
-                    let s = serde_json::to_string_pretty(&mirai)?;
-                    f.write_all(s.as_bytes()).await?;
-                    p.pop();
                 }
 
                 let token = client.gen_token().await;
                 write_token_file(&token, &p).await?;
             }
-            _ => println!("Unknown command '{}', use 'help' to show the help info", first)
+            _ => println!(
+                "Unknown command '{}', use 'help' to show the help info",
+                first
+            ),
         }
     }
 
@@ -287,11 +277,7 @@ async fn device_or_default<P: AsRef<Path>>(dir: P) -> Device {
 }
 
 async fn get_client(device: Device) -> io::Result<Arc<Client>> {
-    let client = Client::new(
-        device,
-        version::ANDROID_WATCH,
-        DefaultHandler,
-    );
+    let client = Client::new(device, version::ANDROID_WATCH, DefaultHandler);
     let client = Arc::new(client);
 
     //let addr = SocketAddr::new(Ipv4Addr::new(113, 96, 18, 253).into(), 80);
@@ -299,7 +285,9 @@ async fn get_client(device: Device) -> io::Result<Arc<Client>> {
     let stream = socket.connect(client.get_address()).await?;
 
     let client0 = client.clone();
-    tokio::spawn(async move { client0.start(stream).await; });
+    tokio::spawn(async move {
+        client0.start(stream).await;
+    });
     yield_now().await;
 
     Ok(client)
@@ -327,22 +315,21 @@ fn get_qr<B: AsRef<[u8]>>(b: B) -> Result<String, Box<dyn Error>> {
     let s = grids[0].decode()?.1;
 
     let qr = QrCode::new(s.as_bytes())?;
-    Ok(
-        qr.render::<unicode::Dense1x2>()
+    Ok(qr
+        .render::<unicode::Dense1x2>()
         .dark_color(unicode::Dense1x2::Light)
         .light_color(unicode::Dense1x2::Dark)
-        .build()
-    )
+        .build())
 }
 
 #[cfg(test)]
 mod test {
 
-    use qrcode::QrCode;
     use qrcode::render::unicode;
+    use qrcode::QrCode;
 
+    use crate::compat::mirai::MiraiDeviceInfo;
     use ricq::device::Device;
-    use crate::compat::mirai::{MiraiDeviceInfo};
 
     #[test]
     fn qr() {
@@ -354,7 +341,8 @@ mod test {
         let s = grids[0].decode().unwrap().1;
 
         let qr = QrCode::new(s.as_bytes()).unwrap();
-        let s = qr.render::<unicode::Dense1x2>()
+        let s = qr
+            .render::<unicode::Dense1x2>()
             .dark_color(unicode::Dense1x2::Light)
             .light_color(unicode::Dense1x2::Dark)
             .build();
@@ -364,22 +352,7 @@ mod test {
     #[test]
     fn byte_8() {
         let a = [
-            12u8,
-            64,
-            241,
-            114,
-            202,
-            68,
-            189,
-            13,
-            133,
-            122,
-            67,
-            241,
-            146,
-            140,
-            247,
-            134
+            12u8, 64, 241, 114, 202, 68, 189, 13, 133, 122, 67, 241, 146, 140, 247, 134,
         ];
 
         let mut str = String::new();
@@ -395,8 +368,6 @@ mod test {
     fn d_to_mirai() {
         let d = Device::random();
         let mirai: MiraiDeviceInfo = d.into();
-
-
 
         println!("{:#?}", mirai);
     }
@@ -438,38 +409,5 @@ mod test {
 
         let mirai_device: MiraiDeviceInfo = serde_json::from_str(mirai_device).unwrap();
         println!("{:#?}", mirai_device);
-    }
-}
-
-struct CliLogger;
-
-impl Subscriber for CliLogger {
-    fn enabled(&self, metadata: &Metadata<'_>) -> bool {
-        todo!()
-    }
-
-    fn new_span(&self, span: &Attributes<'_>) -> Id {
-        todo!()
-    }
-
-    fn record(&self, span: &Id, values: &Record<'_>) {
-        todo!()
-    }
-
-    fn record_follows_from(&self, span: &Id, follows: &Id) {
-        todo!()
-    }
-
-    fn event(&self, event: &Event<'_>) {
-
-        todo!()
-    }
-
-    fn enter(&self, span: &Id) {
-        todo!()
-    }
-
-    fn exit(&self, span: &Id) {
-        todo!()
     }
 }
